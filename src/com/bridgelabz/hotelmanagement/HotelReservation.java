a2b55cfc3afda8c8bf84a69ab8dd3475fbb35753
riya40 <priyanunna20@gmail.com>
2021-10-26 08:33:19 +0530

    Added:Welcome Message For Hotel Management

---
+++ src/com/bridgelabz/hotelmanagement/HotelReservation.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.hotelmanagement;
+
+public class HotelReservation {
+
+    public static void main(String[] args) {
+        //Displaying the Welcome Message
+        System.out.println("Welcome to Hotel Reservation System");
+    }
+}
